962097a9b4afab02bd66f214d0b14f5f09e9b33b
Aritz Lopez <aritzhack@gmail.com>
2017-10-18 12:53:04 +0200

    Fixed credits

---
+++ credits.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php
+    require_once "parts/head.php";
+    ?>
+<body>
+<?php
+require_once "parts/header.php";
+?>
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 center-align">
+                <h1>Credits</h1>
+            </div>
+            <div class="col s12 center-align">
+                <h2>Authors</h2>
+            </div>
+
+            <div class="col s12 center-align">
+                <ul style="list-style: none">
+                    <li style="display: inline-block; padding: 16px; text-align: center; border-right: 1px solid gray">
+                        <img src="/img/elena.png"><br>
+                        Elena Hernandez <br>
+                        <i class="material-icons">phone</i> 666-666-666 <br>
+                        <i class="material-icons">email</i> ehernandez035@ikasle.ehu.eus
+                    </li>
+                    <li style="display: inline-block; padding: 16px; text-align: center">
+                        <img src="/img/aritz.png"><br>
+                        Aritz Lopez <br>
+                        <i class="material-icons">phone</i> 666-666-666 <br>
+                        <i class="material-icons">email</i> alopez306@ikasle.ehu.eus
+                    </li>
+                </ul>
+            </div>
+            <div class="col s12 center-align">
+                <h2>Speciality</h2>
+                Computation
+            </div>
+        </div>
+    </div>
+</main>
+
+
+<?php
+require_once "parts/footer.php";
+?>
+</body>
+</html>
