150c7f98d519bb03e9bae86a740c5b13007357e8
Aritz Lopez <aritzhack@gmail.com>
2017-10-18 12:51:39 +0200

    Fixed link

--- parts/header.php
+++ parts/header.php
@@ -1,13 +1,9 @@
 <header class='main' id='h1'>
-    <ul id="dropdown1" class="dropdown-content">
-        <li><a href="/addQuestion.html">HTML4 + jQuery</a></li>
-        <li><a href="/addQuestion5.html">HTML5</a></li>
-    </ul>
     <nav>
         <div class="nav-wrapper" style="padding-left: 32px">
-            <a href="/layout.html" class="brand-logo">Quizzes</a>
+            <a href="/layout.php" class="brand-logo">Quizzes</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="/layout.html">Home</a></li>
+                <li><a href="/layout.php">Home</a></li>
                 <li><a href="/addQuestion5.php">Add quiz</a></li>
                 <li><a href="/showQuestions.php">Show quizzes</a></li>
                 <li><a href="/credits.html">Credits</a></li>
